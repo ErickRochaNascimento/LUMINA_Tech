417d867ea5c04a35ec79483b125404f838652e06
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-30 23:22:34 -0300

    Adição header e footer na pagina meus pedidos

--- src/app/pages/meus-pedidos/meus-pedidos.component.ts
+++ src/app/pages/meus-pedidos/meus-pedidos.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SalesService } from '../../services/sales.service';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-meus-pedidos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './meus-pedidos.component.html',
   styleUrl: './meus-pedidos.component.css'
 })
