dff04d84e858be3dfbf8774b0ded159505c4daed
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-12-17 21:16:44 -0300

    Conserto dos formularios

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -17,15 +17,19 @@ import { FooterComponent } from "../../components/footer/footer.component";
 export class CadastroComponent {
   auth = inject(AuthService);
   router = inject(Router);
-  dados = { nome: '',sobrenome: '', email: '', senha: '' };
+  dados = { nome: '', sobrenome: '', email: '', senha: '' };
   aceitouTermos = false;
 
   registrar() {
-    if (this.auth.cadastrar(this.dados)) {
-      alert('Cadastro realizado! Faça login.');
-      this.router.navigate(['/login']);
-    } else {
-      alert('Este e-mail já está em uso.');
+    if (!this.dados.nome || !this.dados.sobrenome || !this.dados.email || this.dados.senha.length < 6) {
+      alert('Por favor, preencha todos os campos corretamente. A senha deve ter no mínimo 6 dígitos.');
+      return;
+      if (this.auth.cadastrar(this.dados)) {
+        alert('Cadastro realizado! Faça login.');
+        this.router.navigate(['/login']);
+      } else {
+        alert('Este e-mail já está em uso.');
+      }
     }
   }
 }
